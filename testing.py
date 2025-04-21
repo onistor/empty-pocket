6b9e3b495c4df41340b04e579438094868b01559
onistor <rost.cool@gmail.com>
2025-04-21 13:00:48 +0300

    test lesson

--- testing.py
+++ testing.py
@@ -1,8 +1,19 @@
+# N = int(input())
+# c=0
+# my_list=[]
+# while N>c:
+#     c=c+1
+#     my_list.append(input())
+# else:
+#     print(my_list)
+
 N = int(input())
-c=0
-my_list=[]
-while N>c:
-    c=c+1
-    my_list.append(input())
+c = 2
+while c < N:
+    if N % c != 0:
+        c = c + 1
+        continue
+    else: print('False')
+    break
 else:
-    print(my_list)
+    print('True')
